095725caab4b604f65e6257e256ed43fee0d964a
karenca01 <kcervantes01@alumnos.uaq.mx>
2025-10-29 19:58:30 -0600

    Initial sidebar

---
+++ app/dashboard/_components/EventSearchBar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { Input, Button } from "@heroui/react";
+import { LuSearch } from "react-icons/lu";
+
+export default function EventSearchBar({ onSearch }: { onSearch: (id: string) => void }) {
+  const [eventId, setEventId] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // evita que recargue la página
+    if (!eventId.trim()) return;
+    onSearch(eventId);
+    setEventId("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center gap-2 w-full max-w-md mx-auto mt-6"
+    >
+      <Input
+        type="text"
+        label="Buscar evento por ID"
+        placeholder="Ejemplo: EVT-1023"
+        value={eventId}
+        onChange={(e) => setEventId(e.target.value)}
+        className="flex-1"
+      />
+    </form>
+  );
+}
--- app/dashboard/_components/Sidebar.tsx
+++ app/dashboard/_components/Sidebar.tsx
@@ -1,11 +1,26 @@
-import { LuBuilding, LuCalendar, LuStore, LuTruck, LuUser, LuUsers, LuWheat } from "react-icons/lu";
+import { LuBuilding, LuCalendar } from "react-icons/lu";
 import NavItem from "./NavItem";
+import EventSearchBar from "./EventSearchBar";
 
 export default function Sidebar(){
+    const handleEventSearch = async (id: string) => {
+        console.log("Buscando evento con ID:", id);
+    }
+
     return(
-        <nav className="w-1/12 h-[90vh] bg-orange-200 flex flex-col items-center py-20 justify-center gap-10">
-            <NavItem icon={<LuBuilding className="text-4xl"/>} path="/dashboard"/>
-            <NavItem icon={<LuCalendar className="text-4xl"/>} path="/dashboard/users/"/>
+        <nav className="w-3/12 h-[90vh] bg-gris-claro flex flex-col items-center py-20 justify-center gap-10">
+            <div className="w-3/4">
+                <EventSearchBar onSearch={handleEventSearch}/>
+            </div>
+            <div className="flex flex-row justify-center items-center gap-4">
+                <NavItem icon={<LuBuilding className="text-4xl w-30 border-1"/>} path="/dashboard"/>
+                <NavItem icon={<LuCalendar className="text-4xl w-30 border-1"/>} path="/dashboard/users/"/>
+            </div>
+            <div className="flex flex-row justify-center items-center gap-4">
+                <NavItem icon={<LuCalendar className="text-4xl w-30 border-1"/>} path="/dashboard/users/"/>
+                <NavItem icon={<LuBuilding className="text-4xl w-30 border-1"/>} path="/dashboard"/>
+            </div>
+            
             //hacer modal para crear evento
         </nav>
     )
